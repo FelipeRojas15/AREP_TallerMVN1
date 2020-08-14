0a5fefc3ba3e86806a135bd739bd25c26eaff94c
FelipeRojas15 <53972640+FelipeRojas15@users.noreply.github.com>
2020-08-13 23:21:49 -0500

    Update src, informe

--- src/main/java/edu/escuelaing/arem/app/App.java
+++ src/main/java/edu/escuelaing/arem/app/App.java
@@ -13,9 +13,15 @@ public class App {
     public static void main(String[] args){
     DataReader data = new DataReader();
     LinkedList lista = data.readFile("data.txt");
+    LinkedList listaDos  = data.readFile("dataDos.txt");
     Calculator operation = new Calculator(lista);
+    Calculator operacion = new Calculator(listaDos);
+    System.out.println("#Case 1");
     System.out.printf("La media es: %.2f %n",operation.mean());
     System.out.printf("La desviacion estandar es: %.2f %n",operation.standarDeviation());
+    System.out.println("#Case 2");
+    System.out.printf("La media es: %.2f %n",operacion.mean());
+    System.out.printf("La desviacion estandar es: %.2f %n",operacion.standarDeviation());
     }
     
 }
